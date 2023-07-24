6929d43acb893c5d1c9647d11d116681c931d26c
PhoenixMomof2 <89948037+PhoenixMomof2@users.noreply.github.com>
2023-07-24 08:22:41 -0400

    modified next.config.js file

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+  images: {
+    domains: [], // Add the domain(s) from which you are fetching images, e.g., ["example.com"]
+    loader: 'default', // Use "default" loader for regular images or add other loaders like "imgix" or "cloudinary"
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
